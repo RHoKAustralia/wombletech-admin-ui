dd36b52275a8e4e2d50e67b0d966ccec5ac7c057
András Bubics <abubics@dius.com.au>
2021-10-10 18:56:46 +1100

    Add OIDC callback hooks

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,13 +1,34 @@
+import { useEffect } from 'react'
 import { useCases } from 'config/deps'
 import { DonationList, Header } from './presentation'
 
 import './index.css'
 
-const App = () => (
-  <div className='App'>
-    <Header />
-    {useCases.auth.loggedIn() ? <DonationList /> : null}
-  </div>
-)
+const codeFromUrl = () => new URL(window.location).searchParams.get('code')
+
+const removeCodeFromUrl = () => {
+  window.history.replaceState({}, window.title, window.location.origin)
+}
+
+const tryLogin = async () => {
+  const code = codeFromUrl()
+  if (code) {
+    try {
+      await useCases.auth.logInByCode(code)
+      removeCodeFromUrl()
+    } catch (e) {}
+  }
+}
+
+const App = () => {
+  useEffect(tryLogin)
+
+  return (
+    <div className='App'>
+      <Header />
+      {useCases.auth.loggedIn() ? <DonationList /> : null}
+    </div>
+  )
+}
 
 export default App
